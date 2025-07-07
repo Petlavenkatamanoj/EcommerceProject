1795b9a6cf19fc5f0cbcb8126597e78bc6d39de2
manoj <petlavenkatamanoj@gmail.com>
2025-07-07 12:48:50 +0530

    updated few field in the project

--- ecom-proj/src/main/java/com/example/ecom_proj/repo/ProductRepo.java
+++ ecom-proj/src/main/java/com/example/ecom_proj/repo/ProductRepo.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product,Integer> {
 
-    @Query("SELECT p from product p WHERE" +"LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword ,'%')) OR" +
-    "LOWER(p.decription) LIKE LOWER(CONCAT('%', :keyword,'%)) OR" +"LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword,'%)) OR" +
-            "LOWER(p.decription) LIKE LOWER(CONCAT('%', :keyword,'%'))")
+    @Query("SELECT p FROM Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     public List<Product> searchProduct(String keyword);
 }
